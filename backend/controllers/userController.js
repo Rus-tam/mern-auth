8a46db4d0a65c30c12c231f3c1dd0de54bdc0534
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-07-18 18:06:45 +0500

    custom error middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,13 @@
+import asyncHandler from "express-async-handler";
+
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
+  res.status(401);
+  throw new Error("Something went wrong");
+
   res.status(200).json({ message: "Auth user" });
-};
+});
 
 export { authUser };
