b4c4ee055e3357c612955a30072366c4f8035940
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2023-07-21 13:51:29 +0500

    connected db

--- backend/db/sequelize.js
+++ backend/db/sequelize.js
@@ -1,13 +1,16 @@
 import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const sequelize = new Sequelize(
   process.env.POSTGRES_DATABASE,
   process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
+  process.env.POSTGRES_PASSWORD.toString(),
   {
     host: process.env.POSTGRES_HOST,
     dialect: "postgres",
-    port: process.env.PORT,
+    port: process.env.POSTGRES_PORT,
   },
 );
 
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
 import userRoutes from "./routes/userRoutes.js";
+import User from "./db/User.js";
 
 const app = express();
 
@@ -14,5 +15,16 @@ app.use(notFound);
 app.use(errorHandler);
 
 // app.get("/", (req, res) => res.send("Server is ready"));
+
+(async () => {
+  try {
+    await User.sync({ alter: true });
+    console.log("Tables created successfully");
+  } catch (err) {
+    console.error(`Error creating tables: ${err}`);
+  }
+})();
+
+console.log(process.env.POSTGRES_PASSWORD);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
